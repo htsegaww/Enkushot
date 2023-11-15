dcbe2d475224eb3c81bd78d5fede82ee49a04268
Henok <hennok100@gmail.com>
2023-11-14 21:59:57 -0500

    added .env file

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -6,12 +6,12 @@ import { getFirestore, serverTimestamp } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnaf05SQCfYpxjU18ZP_l3y-H0ZBaAkbI",
-  authDomain: "henogram-4f9e9.firebaseapp.com",
-  projectId: "henogram-4f9e9",
-  storageBucket: "henogram-4f9e9.appspot.com",
-  messagingSenderId: "329948263375",
-  appId: "1:329948263375:web:ed8824b2161442b63318ba",
+  apiKey: import.meta.env.REACT_APP_API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_PROJECTED_ID,
+  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_REACT_APP_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
