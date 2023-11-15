04965372bd946357fe5edb135bf4247b66ec21e5
Henok <hennok100@gmail.com>
2023-11-14 22:18:49 -0500

    edited .env file

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <div className="icon-title"></div>
       <h2>Create Your own Memories...📸</h2>
       <p>
         feel free to add your own picture(let's show our own photography
